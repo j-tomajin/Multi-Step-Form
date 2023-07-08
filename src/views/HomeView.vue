f7834a860fd082b10b3dc5e8de308ee159066df6
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 01:09:07 +0800

    Home View

---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script>
+  import { RouterLink } from 'vue-router'
+  export default {
+    name: 'HomeView',
+    data() {
+      return {
+        text: '',
+        isDisable: true,
+      }
+    },
+    methods: {
+      clickMe() {
+        if(this.text === '') {
+          alert('blank')
+          this.isDisable = true
+        } else {
+          this.isDisable = false
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Home VIew</h1>
+    <input type="text" v-model="text">
+    <router-link :to="isDisable ? '' : {name: 'plan'}">
+      <button @click="clickMe()">Plan</button>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
