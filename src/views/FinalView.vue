c6719c09bcd3f2af69504460b4c337812802ec58
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 01:09:00 +0800

    final view

---
+++ src/views/FinalView.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'FinalView',
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Final View</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
