b6e54b027cd8ea0c9a7d532f4d4acd834a4531b2
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 01:08:37 +0800

    router

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory } from "vue-router"
+
+import HomeView from '../views/HomeView.vue'
+import PlanView from '../views/PlanView.vue'
+import AddOnView from '../views/AddOnView.vue'
+import FinalView from '../views/FinalView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+          path: '/',
+          name: 'home',
+          component: HomeView
+        },
+        {
+          path: '/plan',
+          name: 'plan',
+          component: PlanView
+        },
+        {
+          path: '/add-ons',
+          name: 'addOn',
+          component: AddOnView
+        },
+        {
+          path: '/finishing-up',
+          name: 'final',
+          component: FinalView
+        },
+        {
+            path: '/:patchMatch(.*)*',
+            name: 'notFound',
+            component: NotFoundView
+        }
+    ]
+})
+
+export default router
