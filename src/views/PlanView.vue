3f4f7b0f7e0ea28fba2ed37b5e6047e6ce56e472
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 01:09:18 +0800

    plan view

---
+++ src/views/PlanView.vue
@@ -0,0 +1,20 @@
+<script>
+  import { RouterLink } from 'vue-router'
+
+  export default {
+    name: 'PlanView',
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Plan View</h1>
+    <router-link :to="{name: 'addOn'}">
+      <button>Add Ons</button>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
