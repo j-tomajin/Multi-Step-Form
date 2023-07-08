16342e14333582e236ee8078ec634fc27e4ab57a
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 01:08:52 +0800

    Add ons view

---
+++ src/views/AddOnView.vue
@@ -0,0 +1,20 @@
+<script>
+  import { RouterLink } from 'vue-router'
+
+  export default {
+    name: 'AddOnView',
+  }
+</script>
+
+<template>
+  <div>
+    <h1>Add ons view</h1>
+    <router-link :to="{name: 'final'}">
+      <button>Final</button>
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use '../assets/scss/utilities/' as *;
+</style>
