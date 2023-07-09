011bf6b99284486b5c0d231553f3b475ea53792c
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 22:32:51 +0800

    added thank you view page

--- src/router/index.js
+++ src/router/index.js
@@ -4,37 +4,43 @@ import HomeView from '../views/HomeView.vue'
 import PlanView from '../views/PlanView.vue'
 import AddOnView from '../views/AddOnView.vue'
 import FinalView from '../views/FinalView.vue'
+import ThankYouView from '../views/ThankYouView.vue'
 import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-          path: '/',
-          name: 'home',
-          component: HomeView
-        },
-        {
-          path: '/plan',
-          name: 'plan',
-          component: PlanView
-        },
-        {
-          path: '/add-ons',
-          name: 'addOn',
-          component: AddOnView
-        },
-        {
-          path: '/finishing-up',
-          name: 'final',
-          component: FinalView
-        },
-        {
-            path: '/:patchMatch(.*)*',
-            name: 'notFound',
-            component: NotFoundView
-        }
-    ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: HomeView
+      },
+      {
+        path: '/plan',
+        name: 'plan',
+        component: PlanView
+      },
+      {
+        path: '/add-ons',
+        name: 'addOn',
+        component: AddOnView
+      },
+      {
+        path: '/finishing-up',
+        name: 'final',
+        component: FinalView
+      },
+      {
+        path: '/thank-you',
+        name: 'thankYou',
+        component: ThankYouView
+      },
+      {
+          path: '/:patchMatch(.*)*',
+          name: 'notFound',
+          component: NotFoundView
+      }
+  ]
 })
 
 export default router
