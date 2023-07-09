b2855dbac06909258de769e1e04d651d2b5ab14f
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-09 22:33:04 +0800

    Pinia Store

---
+++ src/store/FormStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+
+export const useFormStore = defineStore('form-store', {
+  state: () => {
+    return { 
+      // states here 
+    }
+  },
+  getters: {
+    // getters here
+  },
+  actions: {
+    // actions here
+  }
+})
