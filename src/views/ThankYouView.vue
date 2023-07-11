7ac7a0cfa75ff78785d5f91cd69ff9b374cbb3ae
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-11 22:45:09 +0800

    thank you vie

--- src/views/ThankYouView.vue
+++ src/views/ThankYouView.vue
@@ -1,15 +1,63 @@
 <script>
+  import thankyouIcon from '../assets/images/icon-thank-you.svg'
   export default {
     name: '',
+    data() {
+      return {
+        icon: thankyouIcon,
+      }
+    }
   }
 </script>
 
 <template>
-  <div>
-    <h1>ThankYouView</h1>
+  <div class="container box">
+    <div class="icon">
+      <img :src="icon" alt="check icon">
+    </div>
+
+    <h1 class="heavy">Thank You</h1>
+    <p class="medium">
+      Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at <span><a href="">support@loremgaming.com</a></span>.
+    </p>
   </div>
 </template>
 
 <style lang="scss" scoped>
   @use '../assets/scss/utilities/' as *;
+  
+  .icon {
+    margin-bottom: rem(16);
+    img {
+      margin-inline: auto;
+    }
+  }
+
+  .box {
+    text-align: center;
+  }
+
+  a {
+    color: var(--clr-primary-purplish-blue);
+    position: relative;
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: -2px;
+      left: 0;
+      right: 0;
+
+      margin-inline: auto;
+      background-color: var(--clr-primary-purplish-blue);
+      width: 0;
+      height: 2px;
+
+      transition: width 300ms ease-in-out;
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+  }
 </style>
