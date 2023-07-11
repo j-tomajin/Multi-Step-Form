61d3fc92516ff1f8285cc248a36c68424d39b0c5
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-11 22:44:43 +0800

    add-ons

--- src/views/AddOnView.vue
+++ src/views/AddOnView.vue
@@ -9,6 +9,7 @@
       return {
         addOns: [
           {
+            id: 1,
             title: 'Online service',
             desc: 'Access to multiplayer games',
             price: {
@@ -18,6 +19,7 @@
             checked: false,
           },
           {
+            id: 2,
             title: 'Larger storage',
             desc: 'Extra 1TB of cloud save',
             price: {
@@ -27,6 +29,7 @@
             checked: false,
           },
           {
+            id: 3,
             title: 'Customizable Profile',
             desc: 'Custom theme on your profile',
             price: {
@@ -35,24 +38,38 @@
             },
             checked: false,
           },
-        ]
+        ],
+        isValid: false,
       }
     },
     setup() {
       const formStore = useFormStore()
 
-      const { isChecked } = storeToRefs(formStore)
+      const { isChecked, addOnStore, plansStore } = storeToRefs(formStore)
+      const { getTotal } = formStore
 
       const router = useRouter()
 
       return { router,
         // states
-        isChecked,
+        isChecked, addOnStore,
+
+        // actions
+        getTotal, plansStore
       }
     },
-    methdos: {
-      checked() {
-
+    methods: {
+      checkedF(id) {
+        this.addOns = this.addOns.map((ao) => ao.id === id ? { ...ao, checked: !ao.checked } : ao)
+      },
+      
+      valAdd() {
+        this.addOnStore = this.addOns.filter((ao) => ao.checked)
+        if(this.addOnStore.length === 0) {
+          this.isValid = false
+        } else {
+          this.isValid = true 
+        }
       }
     }
   }
@@ -71,14 +88,17 @@
       <fieldset>
         <legend class="sr-only">CHECKBOX</legend>
 
-        <div v-for="(ao, i) in addOns" :key="i" :class="[ao.checked ? 'active' : '', 'form-field']">
-          <input type="checkbox" :id="`checkbox${i}`" class="sr-only">
-          <label :for="`checkbox${i}`" @click="ao.checked = !ao.checked" >
+        <div v-for="ao in addOns" :key="ao.id" :class="[ao.checked ? 'active' : '', 'form-field']">
+          <input type="checkbox" :id="`checkbox${ao.id}`" class="sr-only">
+          <label :for="`checkbox${ao.id}`" @click="checkedF(ao.id)" >
             <div class="">
-              <p>{{ ao.title }} </p>
-              <p>{{ ao.desc }}</p>
-              <p v-if="isChecked">{{ ao.price.yr }}</p>
-              <p v-else>{{ ao.price.mo }}</p>
+              <h2 class="heavy">{{ ao.title }}</h2>
+              <p class="medium">{{ ao.desc }}</p>
+            </div>
+
+            <div class="price light">
+              <h3 v-if="isChecked">${{ ao.price.yr }}/yr</h3>
+              <h3 v-else>${{ ao.price.mo }}/mo</h3>
             </div>
           </label>
         </div>
@@ -88,8 +108,8 @@
     <!-- BUTTON ROUTES -->
     <div class="-gbtn">
       <button @click="router.back()" class="back-btn">Go Back</button>
-      <router-link :to="{name: 'final'}" class="btn">
-        <button>Next Step</button>
+      <router-link :to="isValid ? {name: 'final'} : ''" class="btn">
+        <button @click="valAdd(), getTotal()">Next Step</button>
       </router-link>
     </div>
   </div>
@@ -106,9 +126,12 @@
 
     fieldset {
       border: 0;
-      display: grid;
-      grid-template-columns: repeat(1, 1fr);
+      display: flex;
+      flex-direction: column;
+      justify-content: center;      
       gap: rem(16);
+      padding: 0;
+      margin: 0;
     }
   }
 
@@ -124,7 +147,21 @@
       align-items: center;
       gap: rem(12);
       border-radius: rem(3);
+
+      h2 {
+        font-size: clampf(18, 780, 24);
+        margin-bottom: rem(8);
+      }
+
+      h3 {
+        font-weight: 400;
+      }
+
+      .price {
+        margin-left: auto;
+      }
     }
+
     label::before {
       content: '';
       display: block;
