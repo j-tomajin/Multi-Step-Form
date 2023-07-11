625f68155535627b36fe07b5355ee97250d2c2b9
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-11 22:44:26 +0800

    modified strore

--- src/store/FormStore.js
+++ src/store/FormStore.js
@@ -1,11 +1,24 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useFormStore = defineStore('form-store', {
   state: () => {
     return { 
       // states here 
-      isChecked: false,
-      plansStore: []
+      isChecked: ref(false),
+      plansStore: ref([
+        {
+          icon: '',
+          title: 'asd',
+          price: {
+            mo: 0,
+            yr: 0,
+          }
+        }
+      ]),
+      addOnStore: ref([]),
+      totalMo: ref(0),
+      totalYr: ref(0),
     }
   },
   getters: {
@@ -15,6 +28,22 @@ export const useFormStore = defineStore('form-store', {
     // actions here
     check() {
       this.isChecked = !this.isChecked
+    },
+    getTotal() {
+      let AddOnTotalMo = this.addOnStore.map(ao => ao.price.mo).reduce((prev, cur) => prev + cur, 0)
+      let planTotalMo = this.plansStore.map(p => p.price.mo).reduce((prev, cur) => prev + cur, 0)
+
+      let AddOnTotalYr = this.addOnStore.map(ao => ao.price.yr).reduce((prev, cur) => prev + cur, 0)
+      let planTotalYr = this.plansStore.map(p => p.price.yr).reduce((prev, cur) => prev + cur, 0)
+
+
+      this.totalMo = eval(
+        planTotalMo + AddOnTotalMo
+      )
+      
+      this.totalYr = eval(
+        planTotalYr + AddOnTotalYr
+      )
     }
   }
 })
