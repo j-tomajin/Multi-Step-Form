411349ffe4716955c4e70608e536115c814359c9
Jerickson Tomajin <tomajinj@gmail.com>
2023-07-12 20:31:07 +0800

    added steps component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script>
   import { RouterView } from 'vue-router';
+  import Steps from './components/Steps.vue';
   export default {
     name: 'App',
+    components: { Steps }
   }
 </script>
 
 <template>
-  <main>
+  <main class="main">
+    <Steps />
     <router-view v-slot="{ Component }">
       <keep-alive>
         <component :is="Component" />
@@ -18,7 +21,21 @@
 <style lang="scss" scoped>
   @use './assets/scss/utilities/' as *;
 
-  main {
+  .main {
+    margin-inline: auto;
+    width: min(calc(100% - 32px), rem(950));
     font-weight: 700;
+    border-radius: rem(16);
+
+    @include breakpointMin(medium-screen) {
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      gap: rem(16);
+      padding: rem(16);
+      background-color: var(--clr-neutral-w);
+      overflow: hidden;
+      height: rem(600);
+      box-shadow: 0px 8px 14px -13px rgba(8, 8, 74, 1);
+    }
   }
 </style>
